2010e5c73db69b500dab909ff962c9178e17474e
Juan Carlos Ruvalcaba <juancarlosruvalcabaflores79@gmail.com>
2025-02-05 14:33:42 -0600

    Styling fixes

--- src/pages/IndexReports.vue
+++ src/pages/IndexReports.vue
@@ -6,10 +6,10 @@
             </div>
         </div>
         <div class="row justify-center q-gutter-xl q-px-lg ">
-            <q-btn color="purple" size="xl" class="col-3" label="PG520" @click="$router.push('/other_models/2G520')" />
-            <q-btn color="purple" size="xl" class="col-3" label="P2312" @click="$router.push('/other_models/2312')" />
-            <q-btn color="primary" size="xl" class="col-3" label="ICT" @click="$router.push('/ICT')" />
-            <q-btn color="primary" size="xl" class="col-3" label="MDA" @click="$router.push('/MDA')" />
+            <q-btn color="purple" size="xl" class="col-4" label="PG520" @click="$router.push('/other_models/2G520')" />
+            <q-btn color="purple" size="xl" class="col-4" label="P2312" @click="$router.push('/other_models/2312')" />
+            <q-btn color="primary" size="xl" class="col-4" label="ICT" @click="$router.push('/ICT')" />
+            <q-btn color="primary" size="xl" class="col-4" label="MDA" @click="$router.push('/MDA')" />
         </div>
     </q-page>
 </template>

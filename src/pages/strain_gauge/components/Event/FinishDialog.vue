2be936f9c1292ecac0be21466d95e7a6ad72c713
Juan Carlos Ruvalcaba <juancarlosruvalcabaflores79@gmail.com>
2025-02-13 14:51:39 -0600

    Compiler fixes

--- src/pages/strain_gauge/components/Event/FinishDialog.vue
+++ src/pages/strain_gauge/components/Event/FinishDialog.vue
@@ -36,11 +36,6 @@
 import { api } from 'boot/axios'
 
 export default {
-  setup(props) {
-    return {
-      props
-    }
-  },
   props: ['event_id'],
   emits: ['reload'],
   data() {
@@ -51,7 +46,10 @@ export default {
       users: [],
       asigned_to: '',
       file: null,
-      sg_report: null
+      sg_report: null,
+
+      //Login variables
+      current_user: {},
     }
   },
   methods: {
@@ -87,16 +85,6 @@ export default {
       })
     },
     finishStrainGauge() {
-      if (this.sg_report == null) {
-        this.$q.notify({
-          message: 'Debes seleccionar un archivo de calibración',
-          type: 'warning',
-          position: 'center',
-          timeout: 2500
-        })
-        return
-      }
-
       const dismiss = this.$q.notify({
         spinner: true,
         message: "Por favor, espera...",
@@ -128,6 +116,7 @@ export default {
         });
 
         this.$emit('reload')
+        this.setFinishSGEmail()
         this.finishDialog = false
 
       }).catch((error) => {
@@ -149,232 +138,124 @@ export default {
         });
 
       })
-    }
-  },
-  mounted() {
+    },
+    setFinishSGEmail() {
 
-  }
-},
-props: ['event_id'],
-  emits: ['reload'],
-    data() {
-  return {
-    finishDialog: false,
-    comments: '',
-    usuarios: [],
-    users: [],
-    asigned_to: '',
-    file: null,
-    sg_report: null,
 
-    //Login variables
-    current_user: {},
-  }
-},
-methods: {
-  openDialog() {
-    this.finishDialog = true;
-    this.getData()
-  },
-  getData() {
-    api.get('/users/testusers').then((response) => {
-      var data = response.data
-      this.usuarios = []
-      data.forEach(dat => {
-        this.usuarios.push({
-          value: dat.ID_USER,
-          label: dat.NAME + " " + dat.LASTNAME
-        })
-      });
-      this.users = this.usuarios
-    }).catch((err) => {
-      console.error(err);
-    })
-  },
-  filterUser(val, update, abort) {
-    if (val === '') {
-      update(() => {
-        this.usuarios = this.users
-      })
-      return
-    }
-    update(() => {
-      const needle = val.toLowerCase()
-      this.usuarios = this.usuarios.filter((v) => v.label.toLowerCase().indexOf(needle) > -1)
-    })
-  },
-  finishStrainGauge() {
-    const dismiss = this.$q.notify({
-      spinner: true,
-      message: "Por favor, espera...",
-      timeout: 0
-    })
+      api.get('/strain_gauge/' + this.props.event_id).then((response) => {
+        var dataEmail = response.data[0];
+        console.log(dataEmail);
 
-    const formData = new FormData()
-    formData.append('event_id', this.props.event_id)
-    formData.append('comments', this.comments)
-    formData.append('asigned_to', this.asigned_to.value)
-    formData.append('sg_report', this.sg_report[0])
+        const configs = {
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
+          }
+        }
 
-    const config = {
-      headers: {
-        'content-type': 'multipart/form-data',
-        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
-      }
-    }
+        const paramsEmail = new URLSearchParams()
+        paramsEmail.append('idAssigned', dataEmail.ASIGNED_TO)
+        paramsEmail.append('idUser', this.current_user.id)
+        paramsEmail.append('senderEmail', this.current_user.email)
+        paramsEmail.append('dateSended', dataEmail.FINISH_DATE)
+        paramsEmail.append('sgNumber', dataEmail.ID_EVENT)
 
-    api.put('/strain_gauge/finish', formData, config).then((response) => {
-      dismiss()
-      response.data.data.forEach(data => {
-        this.$q.notify({
-          type: 'positive',
-          message: data,
-          position: "top",
-          timeout: 5000
-        })
-      });
 
-      this.$emit('reload')
-      this.setFinishSGEmail()
-      this.finishDialog = false
+        api.post('email/finishSG', paramsEmail, configs).then(response => {
 
-    }).catch((error) => {
+          console.log(response.data);
 
-      dismiss()
-      try {
-        var errors = error.response.data.error
-      } catch (err) {
-        console.error(err);
+          this.$q.notify({
+            color: 'positive',
+            message: 'Correo enviado con éxito' || response.data,
+            icon: 'check'
+          })
+
+          this.setNewEmailSG()
+        }).catch(error => {
+
+          if (error.response && error.response.data) {
+            // Mostrar el mensaje de error del servidor
+            this.$q.notify({
+              color: 'negative',
+              message: error.response.data.msg || 'Error desconocido',
+              icon: 'warning',
+            });
+          } else {
+            // En caso de que no haya respuesta, mostrar un mensaje genérico
+            this.$q.notify({
+              color: 'negative',
+              message: 'Hubo un error al enviar el correo',
+              icon: 'error',
+            });
+            console.error('Error al enviar correo:', error);
+
+          }
+        });
+      }).catch((error) => {
         console.error(error);
-      }
-      console.error(errors);
-      errors.forEach(ele => {
-        this.$q.notify({
-          type: 'negative',
-          message: ele.msg,
-          position: "top"
-        })
-      });
+      })
+    },
+    setNewEmailSG() {
+      api.get('/strain_gauge/lastevent').then((response) => {
+        var dataEmailNew = response.data
+        //console.log(dataEmailNew);
+        //var START_DATE = '2025-01-30 09:45:00'
+        const configs = {
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
+          }
+        }
 
-    })
+        const paramsEmail = new URLSearchParams()
+        paramsEmail.append('idAssignedNew', dataEmailNew.ASIGNED_TO)
+        paramsEmail.append('idUser', this.current_user.id)
+        paramsEmail.append('senderEmail', this.current_user.email)
+        paramsEmail.append('dateSendNew', dataEmailNew.START_DATE)
+        //paramsEmail.append('dateSendNew', START_DATE)
+        paramsEmail.append('sgNumberNew', dataEmailNew.ID_EVENT)
+
+
+        api.post('email/startNewSG', paramsEmail, configs).then(response => {
+
+          console.log(response.data);
+
+          this.$q.notify({
+            color: 'positive',
+            message: 'Correo enviado con éxito' || response.data,
+            icon: 'check'
+          })
+
+        }).catch(error => {
+
+          if (error.response && error.response.data) {
+            // Mostrar el mensaje de error del servidor
+            this.$q.notify({
+              color: 'negative',
+              message: error.response.data.msg || 'Error desconocido',
+              icon: 'warning',
+            });
+          } else {
+            // En caso de que no haya respuesta, mostrar un mensaje genérico
+            this.$q.notify({
+              color: 'negative',
+              message: 'Hubo un error al enviar el correo',
+              icon: 'error',
+            });
+            console.error('Error al enviar correo:', error);
+
+          }
+        });
+
+      }).catch((error) => {
+        console.error(error);
+      })
+    }
   },
-  setFinishSGEmail() {
 
-
-    api.get('/strain_gauge/' + this.props.event_id).then((response) => {
-      var dataEmail = response.data[0];
-      console.log(dataEmail);
-
-      const configs = {
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-          'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
-        }
-      }
-
-      const paramsEmail = new URLSearchParams()
-      paramsEmail.append('idAssigned', dataEmail.ASIGNED_TO)
-      paramsEmail.append('idUser', this.current_user.id)
-      paramsEmail.append('senderEmail', this.current_user.email)
-      paramsEmail.append('dateSended', dataEmail.FINISH_DATE)
-      paramsEmail.append('sgNumber', dataEmail.ID_EVENT)
-
-
-      api.post('email/finishSG', paramsEmail, configs).then(response => {
-
-        console.log(response.data);
-
-        this.$q.notify({
-          color: 'positive',
-          message: 'Correo enviado con éxito' || response.data,
-          icon: 'check'
-        })
-
-        this.setNewEmailSG()
-      }).catch(error => {
-
-        if (error.response && error.response.data) {
-          // Mostrar el mensaje de error del servidor
-          this.$q.notify({
-            color: 'negative',
-            message: error.response.data.msg || 'Error desconocido',
-            icon: 'warning',
-          });
-        } else {
-          // En caso de que no haya respuesta, mostrar un mensaje genérico
-          this.$q.notify({
-            color: 'negative',
-            message: 'Hubo un error al enviar el correo',
-            icon: 'error',
-          });
-          console.error('Error al enviar correo:', error);
-
-        }
-      });
-    }).catch((error) => {
-      console.error(error);
-    })
-  },
-  setNewEmailSG() {
-    api.get('/strain_gauge/lastevent').then((response) => {
-      var dataEmailNew = response.data
-      //console.log(dataEmailNew);
-      //var START_DATE = '2025-01-30 09:45:00'
-      const configs = {
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-          'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
-        }
-      }
-
-      const paramsEmail = new URLSearchParams()
-      paramsEmail.append('idAssignedNew', dataEmailNew.ASIGNED_TO)
-      paramsEmail.append('idUser', this.current_user.id)
-      paramsEmail.append('senderEmail', this.current_user.email)
-      paramsEmail.append('dateSendNew', dataEmailNew.START_DATE)
-      //paramsEmail.append('dateSendNew', START_DATE)
-      paramsEmail.append('sgNumberNew', dataEmailNew.ID_EVENT)
-
-
-      api.post('email/startNewSG', paramsEmail, configs).then(response => {
-
-        console.log(response.data);
-
-        this.$q.notify({
-          color: 'positive',
-          message: 'Correo enviado con éxito' || response.data,
-          icon: 'check'
-        })
-
-      }).catch(error => {
-
-        if (error.response && error.response.data) {
-          // Mostrar el mensaje de error del servidor
-          this.$q.notify({
-            color: 'negative',
-            message: error.response.data.msg || 'Error desconocido',
-            icon: 'warning',
-          });
-        } else {
-          // En caso de que no haya respuesta, mostrar un mensaje genérico
-          this.$q.notify({
-            color: 'negative',
-            message: 'Hubo un error al enviar el correo',
-            icon: 'error',
-          });
-          console.error('Error al enviar correo:', error);
-
-        }
-      });
-
-    }).catch((error) => {
-      console.error(error);
-    })
+  mounted() {
+    this.current_user = JSON.parse(localStorage.getItem("userLogin"))
   }
-},
-
-mounted() {
-  this.current_user = JSON.parse(localStorage.getItem("userLogin"))
-}
 }
 </script>
